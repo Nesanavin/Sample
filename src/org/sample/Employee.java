ecd0d6244ac80112f7515be9e954306ca0cac958
Nesanavin <mnesapriyan477@gmail.com>
2022-01-27 11:57:23 +0530

    Sample Java

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,5 +1,9 @@
 package org.sample;
 
 public class Employee {
-
+  public static void main(String[] args) {
+	 System.out.println("Nesan");
+	 System.out.println("Id No:12");
+	 System.out.println("Navin");
+}
 }
